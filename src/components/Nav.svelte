69cb2380cd3fa38deebd179b30a2b966937ec93d
Nicholas LaRosa <nlarosa@diameterhealth.com>
2020-03-30 16:40:13 -0400

    add storyblok

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let segment;
+	export let stories = [];
 </script>
 
 <style>
@@ -26,12 +27,12 @@
 		float: left;
 	}
 
-	[aria-current] {
+	.selected {
 		position: relative;
 		display: inline-block;
 	}
 
-	[aria-current]::after {
+	.selected::after {
 		position: absolute;
 		content: '';
 		width: calc(100% - 1em);
@@ -50,11 +51,13 @@
 
 <nav>
 	<ul>
-		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
+		<li class={segment === undefined ? 'selected' : ''}>
+			<a href='.'> Index </a>
+		</li>
+		{#each stories as story}
+			<li class={story.full_slug === segment ? 'selected' : ''}>
+				<a href={story.full_slug}> {story.name} </a>
+			</li>
+		{/each}
 	</ul>
 </nav>
